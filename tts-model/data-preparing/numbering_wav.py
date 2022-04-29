dbc5869e863498933dd108c363af76b6b226d2e8
seungheon <rumah800@naver.com>
2022-04-29 21:45:31 +0900

    wav 1~n 넘버링 구현

---
+++ tts-model/data-preparing/numbering_wav.py
@@ -0,0 +1,12 @@
+import os
+
+file_path = "D:\\projects\\2022-1-OSSP2-Bigpie-5\\tts-model\\datasets\\ko\\audio"
+file_names = os.listdir(file_path)
+
+i = 1
+for name in file_names:
+    src = os.path.join(file_path, name)
+    dst = str(i) + ".wav"
+    dst = os.path.join(file_path, dst)
+    os.rename(src, dst)
+    i += 1
