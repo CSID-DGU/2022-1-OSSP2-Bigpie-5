6db80837c7f3a9f63389c09d64019895b823b313
seungheon <rumah800@naver.com>
2022-04-29 23:37:16 +0900

    alignment.json 만드는 함수 추가

--- tts-model/data-preparing/numbering_wav.py
+++ tts-model/data-preparing/numbering_wav.py
@@ -8,5 +8,8 @@ for name in file_names:
     src = os.path.join(file_path, name)
     dst = str(i) + ".wav"
     dst = os.path.join(file_path, dst)
-    os.rename(src, dst)
+
+    print(name)
+
+    # os.rename(src, dst)
     i += 1
