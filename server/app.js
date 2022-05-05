06f2728c2c02e9b4ee8456b74b0a8b6e8cde5794
seungheon <rumah800@naver.com>
2022-05-06 00:38:59 +0900

    함수 표기 방식 변경

--- server/app.js
+++ server/app.js
@@ -5,10 +5,10 @@ import multer from 'multer';
 const __dirname = path.resolve();
 
 const _storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: (req, file, cb) => {
     cb(null, './uploads');
   },
-  filename: function (req, file, cb) {
+  filename: (req, file, cb) => {
     cb(null, file.originalname);
   },
 });
