100edbb7276be865c4b0e53c9eb4cbb68ff58237
seungheon <rumah800@naver.com>
2022-05-17 02:27:35 +0900

    음성 합성 자동화 구현

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ import multer from 'multer';
 import util from 'util';
 import child_process from 'child_process';
 import { spawn } from 'child_process';
+import mime from 'mime';
+import fs from 'fs';
 // import spawn from 'await-spawn';
 
 const exec = util.promisify(child_process.exec);
@@ -21,6 +23,8 @@ const _storage = multer.diskStorage({
 const upload = multer({ storage: _storage });
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const sleep = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -28,6 +32,75 @@ const sleep = (ms) => {
 
 app.get('/', async (req, res) => {
   res.sendFile('D:\\projects\\2022-1-OSSP2-Bigpie-5\\server\\index.html');
+});
+
+app.get('/voice', async (req, res) => {
+  let { text } = req.body;
+
+  console.log(text);
+
+  text = '이것은 실화입니다.';
+  let wavPath;
+
+  const synthesize = spawn('python', [
+    'synthesizer.py',
+    '--load_path',
+    'logs/myset_2022-04-09_14-37-43',
+    '--text',
+    `"${text}"`,
+  ]);
+
+  synthesize.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+
+    if (data.indexOf('.wav') != -1) {
+      wavPath = data.toString();
+
+      wavPath = wavPath.slice(wavPath.indexOf('samples'));
+
+      console.log();
+      console.log('yayyyyyyyyyyyyyy');
+    }
+  });
+
+  synthesize.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  synthesize.on('close', (code) => {
+    console.log(`synthesize exited with code ${code}`);
+  });
+
+  await sleep(30000);
+
+  wavPath = wavPath.substr(0, wavPath.length - 2);
+
+  const filepath = `D:/projects/2022-1-OSSP2-Bigpie-5/tts-model/${wavPath}`;
+
+  fs.readFile(filepath, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end(null);
+    } else {
+      console.log(filepath);
+      //console.log(data);
+
+      res.end(data);
+    }
+  });
+
+  // const mimetype = mime.getType(file);
+
+  // res.setHeader('Content-disposition', 'attachment; filename=' + file);
+  // res.setHeader('Content-type', mimetype);
+
+  // const filestream = fs.createReadStream(file);
+
+  // filestream.pipe(res);
+
+  // res.download(filepath, 'synthesized_voice.wav');
+
+  // res.json({ text });
 });
 
 app.post('/upload', upload.any(), async (req, res) => {
@@ -48,7 +121,7 @@ app.post('/upload', upload.any(), async (req, res) => {
   });
 
   generateData.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
+    console.log(`generateData exited with code ${code}`);
   });
 
   await sleep(10000);
@@ -64,7 +137,7 @@ app.post('/upload', upload.any(), async (req, res) => {
   });
 
   train.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
+    console.log(`train exited with code ${code}`);
   });
 
   //console.log(req.files);
