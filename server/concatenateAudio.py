d4a7d175fe34cd958d983ebfd1e65e603d61bd8a
seungheon <rumah800@naver.com>
2022-05-24 22:46:53 +0900

    에러 수정

--- server/concatenateAudio.py
+++ server/concatenateAudio.py
@@ -11,9 +11,10 @@ last_line = 0
 
 with open('D:\\projects\\2022-1-OSSP2-Bigpie-5\\tts-model\\wav_count.txt', 'r') as f:
         last_line = int(f.readlines()[-1][:-1])
+        print("last_line1:", last_line)
 
-last_line = last_line - 10
-print(last_line)
+last_line = last_line - int(numFiles)
+print("last_line: ", last_line)
 
 
 
@@ -21,6 +22,7 @@ for i in range(0, int(numFiles)):
     print(infiles);
     
     infiles.append("D:\\projects\\2022-1-OSSP2-Bigpie-5\\tts-model\\samples\\" + str(last_line + i) + ".manual.wav")
+    print(infiles);
     
 
 outfile = "D:\\projects\\2022-1-OSSP2-Bigpie-5\\server\\voices\\result.wav"
