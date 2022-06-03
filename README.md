a2f2d2f6ff169f3cd6c7a42a3ac37a88460a3e8f
jimin9320@gmail.com <jimin9320@gmail.com>
2022-06-03 13:54:30 +0900

    안드로이드

---
+++ README.md
@@ -0,0 +1,25 @@
+<h1 style="color:Tomato;">Login,Register and Update in retrofit</h1>
+
+<body style="background-color:#fff;font-family: sans-serif;" contenteditable="false"><b>
+</b>
+
+
+- create table" dr_users"
+- Api are in <a href="https://github.com/Dinesh2510/Login-registration-in-retrofit/blob/master/webservices.zip"> Webservices.zip</a>
+
+### Login-registration-in-retrofit Android App
+
+- Login 
+- Register
+- Update
+</body>
+
+
+Login | Register | 
+--- | --- |
+<img src="https://user-images.githubusercontent.com/46309253/80273266-571bce80-86ee-11ea-973f-8497882f586e.png" alt="Girl in a jacket" width="300" height="600">| <img src="https://user-images.githubusercontent.com/46309253/80273459-ce059700-86ef-11ea-8c13-59f24405c66b.png" alt="retrofit" width="300" height="600">
+
+
+Update | Home | 
+--- | --- |
+<img src="https://user-images.githubusercontent.com/46309253/80273516-4f5d2980-86f0-11ea-8e44-bed58d2927b0.png" alt="Girl in a jacket" width="300" height="600">| <img src="https://user-images.githubusercontent.com/46309253/80273535-774c8d00-86f0-11ea-9fe4-2f7151e23118.png" alt="retrofit" width="300" height="600">
---
+++ app/src/main/java/com/demo/myapp/Response/RestAdapter.java
@@ -0,0 +1,37 @@
+package com.demo.myapp.Response;
+
+import com.demo.loginregisterretrofit.BuildConfig;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RestAdapter {
+
+    public static API createAPI() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(Level.BODY);
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(5, TimeUnit.SECONDS);
+        builder.writeTimeout(10, TimeUnit.SECONDS);
+        builder.readTimeout(30, TimeUnit.SECONDS);
+        if (BuildConfig.DEBUG) {
+            builder.addInterceptor(logging);
+        }
+        builder.cache(null);
+        OkHttpClient okHttpClient = builder.build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API.BaseURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+
+        return retrofit.create(API.class);
+    }
+}
